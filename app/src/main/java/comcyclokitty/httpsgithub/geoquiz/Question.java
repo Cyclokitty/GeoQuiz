c540d1b5283e539eb8511babaf1c38d90342e85d
Laura Vingada <laura@vingada.com>
2017-11-08 09:31:26 -0500

    Initial commit

---
+++ app/src/main/java/comcyclokitty/httpsgithub/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package comcyclokitty.httpsgithub.geoquiz;
+
+/**
+ * Created by lauravingada on 2017-11-07.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
